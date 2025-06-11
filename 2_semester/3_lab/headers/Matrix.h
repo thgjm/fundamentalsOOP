ce71e87207853171e14fc0a3d13891cd8af49b90
Valerie <valeriachelushkina@gmail.com>
2025-06-12 02:50:25 +0300

    merge class implementations and their headers

--- 2_semester/3_lab/headers/Matrix.h
+++ 2_semester/3_lab/headers/Matrix.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <random>
+#include <iomanip>
 
 using namespace std;
 
@@ -26,3 +28,45 @@ public:
     
     friend ostream& operator<<(ostream& os, const Matrix& matrix);
 };
+
+
+Matrix::Matrix(size_t rows, size_t cols) 
+    : rows(rows), cols(cols), data(rows, vector<double>(cols, 0.0)) {}
+
+Matrix::Matrix(const vector<vector<double>>& data)
+    : data(data), rows(data.size()), cols(data[0].size()) {}
+
+void Matrix::randomize() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(0.0, 1.0);
+
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j) {
+            data[i][j] = dis(gen);
+        }
+    }
+}
+
+void Matrix::print() const {
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j) {
+            cout << fixed << setprecision(2) << data[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+bool Matrix::canMultiply(const Matrix& a, const Matrix& b) {
+    return a.getCols() == b.getRows();
+}
+
+ostream& operator<<(ostream& os, const Matrix& matrix) {
+    for (size_t i = 0; i < matrix.rows; ++i) {
+        for (size_t j = 0; j < matrix.cols; ++j) {
+            os << fixed << setprecision(2) << matrix.data[i][j] << " ";
+        }
+        os << "\n";
+    }
+    return os;
+} 
--- 2_semester/3_lab/headers/SequentialMultiplier.h
+++ 2_semester/3_lab/headers/SequentialMultiplier.h
@@ -1,8 +1,29 @@
 #pragma once
 #include "MatrixMultiplier.h"
+#include <stdexcept>
 
 class SequentialMultiplier : public MatrixMultiplier {
 public:
     Matrix multiply(const Matrix& a, const Matrix& b) override;
     const char* getName() const override { return "Sequential"; }
 }; 
+
+Matrix SequentialMultiplier::multiply(const Matrix& a, const Matrix& b) {
+    if (!Matrix::canMultiply(a, b)) {
+        throw invalid_argument("Matrix dimensions are not compatible for multiplication");
+    }
+
+    Matrix result(a.getRows(), b.getCols());
+    
+    for (size_t i = 0; i < a.getRows(); ++i) {
+        for (size_t j = 0; j < b.getCols(); ++j) {
+            double sum = 0.0;
+            for (size_t k = 0; k < a.getCols(); ++k) {
+                sum += a.at(i, k) * b.at(k, j);
+            }
+            result.at(i, j) = sum;
+        }
+    }
+    
+    return result;
+}
