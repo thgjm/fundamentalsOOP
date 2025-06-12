03f81de060792fd80fab7dafcd88338b48be6e5d
Valerie <valeriachelushkina@gmail.com>
2025-06-12 04:06:34 +0300

    let user input values

--- 2_semester/3_lab/src/main.cpp
+++ 2_semester/3_lab/src/main.cpp
@@ -3,6 +3,8 @@
 #include <chrono>
 #include <iomanip>
 #include <memory>
+#include <thread>
+#include <limits>
 
 const size_t MAX_PRINT = 15; //only print matrices if they're smaller than this
 
@@ -31,20 +33,57 @@ void runTest(const Matrix& a, const Matrix& b, MatrixMultiplier& multiplier) {
     }
 }
 
+//helper function to get valid input
+template<typename T>
+T getInput(const string& prompt, T min_val, T max_val) {
+    T value;
+    while (true) {
+        cout << prompt;
+        if (cin >> value && value >= min_val && value <= max_val) {
+            break;
+        }
+        cout << "Invalid input. Please try again.\n";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    return value;
+}
+
 int main() {
+    //get available CPU threads
+    size_t max_threads = thread::hardware_concurrency();
+    cout << "Your CPU has " << max_threads << " available threads.\n\n";
+
+    //get matrix size from user
+    size_t size = getInput<size_t>(
+        "Enter matrix size (1-10000): ", 
+        1, 
+        10000
+    );
+
+    //get number of threads from user
+    size_t numThreads = getInput<size_t>(
+        "Enter number of threads (1-" + to_string(max_threads) + "): ",
+        1,
+        max_threads
+    );
+
+    cout << "\nConfiguration:\n"
+         << "- Matrix size: " << size << "x" << size << "\n"
+         << "- Number of threads: " << numThreads << "\n"
+         << "- Available CPU threads: " << max_threads << "\n\n";
 
     //test matrices
-    const size_t size = 1000; //can be changed to whatever size
     Matrix a(size, size);
     Matrix b(size, size);
     
-    cout << "Initializing matrices of size " << size << "x" << size << "...\n";
+    cout << "Initializing matrices...\n";
     a.randomize();
     b.randomize();
     
     //multipliers
     SequentialMultiplier seqMult;
-    ParallelMultiplier parMult;
+    ParallelMultiplier parMult(numThreads);
     
     cout << "\nRunning sequential multiplication...\n";
     runTest(a, b, seqMult);
